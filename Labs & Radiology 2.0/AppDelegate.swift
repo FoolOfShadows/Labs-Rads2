ffcbbcf317bc2bbb7c386be1eb08e4c29d3526a1
FoolOfShadows <foolofshadows@gmail.com>
2015-01-30 09:30:45 -0600

    Initial Commit

---
+++ Labs & Radiology 2.0/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Labs & Radiology 2.0
+//
+//  Created by Fool on 1/30/15.
+//  Copyright (c) 2015 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
